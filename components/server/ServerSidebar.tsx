707d102f7c1a7713fb6a8b227f3d82956192ecc0
Purnima <purnimavats6789@gmail.com>
2024-08-19 11:13:01 +0530

    server search modal completed

--- components/server/ServerSidebar.tsx
+++ components/server/ServerSidebar.tsx
@@ -20,7 +20,7 @@ const iconMap = {
 const roleIconMap = {
     [MemberRole.GUEST] : null,
     [MemberRole.MODERATOR] : <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500"/>,
-    [MemberRole.ADMIN] : <ShieldAlert className="h-4 w-4 mr-2 text-indigo-500"/>,
+    [MemberRole.ADMIN] : <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>,
 }
 
 const ServerSidebar = async ({serverId}: ServerSidebarProps) => {
