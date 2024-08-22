7b2fe14f9033908ed46e35acb0a99a1c7c321a8a
Purnima <purnimavats6789@gmail.com>
2024-08-22 19:08:52 +0530

    emoji bar completed

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -9,6 +9,8 @@ import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Plus, Smile } from "lucide-react";
 import { Input } from "../ui/input";
 import { useModal } from "@/hooks/use-modal-store";
+import EmojiPicker from "../EmojiPicker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -28,6 +30,7 @@ const ChatInput = ({
     type
 }: ChatInputProps) => {
     const { onOpen } = useModal();
+    const router = useRouter();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -46,6 +49,9 @@ const ChatInput = ({
             });
 
             await axios.post(url, values)
+
+            form.reset();
+            router.refresh();
         } catch (error) {
             console.log(error)
         }
@@ -75,7 +81,9 @@ const ChatInput = ({
                                         {...field}
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <Smile/>
+                                        <EmojiPicker
+                                            onChange={(emoji: string) => field.onChange(`${field.value}${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
